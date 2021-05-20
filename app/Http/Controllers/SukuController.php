b895c0900eac3bae328787b1ebe1b5d7b5e3b5e8
yazidsupriadi <yazidsupriadi14@gmail.com>
2021-05-20 20:35:03 +0700

    boedaya api

--- app/Http/Controllers/SukuController.php
+++ app/Http/Controllers/SukuController.php
@@ -17,7 +17,7 @@ class SukuController extends Controller
     public function detail($id)
     {
         $suku = Suku::where('provinsi_id','=',$id)
-        ->join('makanan','makanan.suku_id','=','suku.id')
+        ->with('makanan','makanan.suku_id','=','suku.id')
         ->get();
         return $suku;
     }
